d876902ad44ad85c33ed10ad03e12556e82d590f
Ulises Pla Rocher <ulises.pla@innova-tsn.com>
2025-11-20 18:42:38 +0100

    sajhdsa

---
+++ app/signIn_page/page.tsx
@@ -0,0 +1,45 @@
+
+
+
+
+
+export default function Home() {
+  return (
+    <div className="h-screen w-screen bg-gray-300">
+
+
+
+        <div className="inline-block ml-48 mt-12">
+            <div className="ml-3 flex gap-2 items-end">
+              <img
+                src="jamspots_icon.png"
+                alt="Jamspots icon"
+                className="h-16"
+              />
+              <p className="text-xs py-3 text-gray-800 font-semibold">
+                Find the next spot to share your sound.
+              </p>
+            </div>
+
+       
+          </div>
+
+
+
+        <div className="flex flex-col justify-center">
+
+          <div className="text-center mt-48">
+  <h3 className="font-bold text-5xl">BIENVENIDO A JAMSPOTS</h3>
+</div>
+
+
+<div className="h-[1.5px] bg-gray-700/50 w-96 mt-1"></div>
+
+
+</div>
+
+
+
+    </div>
+  );
+}
