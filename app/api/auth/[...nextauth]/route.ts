e67d0fdf239f62b5072c4f58e7db3ecd9ff88c8b
UlisesPlaRocher <ulisesplarocher@gmail.com>
2025-11-28 21:14:08 +0100

    sasad

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,6 @@
-import NextAuth, { User } from "next-auth";
-import GoogleProvider from "next-auth/providers/google";
-import { supabaseAdmin } from "@/lib/supabaseAdmin";
+import NextAuth, { User } from 'next-auth';
+import GoogleProvider from 'next-auth/providers/google';
+import { supabaseAdmin } from '@/lib/supabaseAdmin';
 
 export const authOptions = {
   providers: [
@@ -14,53 +14,30 @@ export const authOptions = {
     async signIn({ user }: { user: User }) {
       if (!user.email) return false;
 
-
-
-
-
-
-       const bb = await supabaseAdmin
-    .from("profiles")
-    .select("*")
-    .eq("email", 'uPR@ucm.es')
-    .single(); // returns one row or null
-
-  if (bb.error) {
-    console.error("Error fetching user:", bb.error);
-   
-  }
-
-  console.log("User data:", bb.data);
-
-
-
-
-
-
-
-  
       const { data } = await supabaseAdmin
-        .from("profiles")  // include schema
-        .select("uuid")
-        .eq("email", user.email)
+        .from('profiles') // include schema
+        .select('id')
+        .eq('email', 'banachlindelof@gmail.com')
         .single();
 
       if (!data) {
-        await supabaseAdmin.from("profiles").insert({
+        await supabaseAdmin.from('profiles').insert({
           email: user.email,
           name: user.name ?? null,
           last_login: new Date().toISOString(),
         });
       } else {
-        await supabaseAdmin.from("profiles")
+        console.log('eiiii111');
+        await supabaseAdmin
+          .from('profiles')
           .update({ last_login: new Date().toISOString() })
-          .eq("uuid", data.uuid);
+          .eq('id', data.id);
       }
 
-            return true;
-          },
-        },
-      };
+      return true;
+    },
+  },
+};
 
-      const handler = NextAuth(authOptions);
-      export { handler as GET, handler as POST };
+const handler = NextAuth(authOptions);
+export { handler as GET, handler as POST };
