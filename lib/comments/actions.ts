1d760264915fcaa6e7a50471e7ad7a6f84ecd0e6
Ulises Pla Rocher <ulises.pla@innova-tsn.com>
2026-07-15 15:20:29 +0200

    asdsagdag

--- lib/comments/actions.ts
+++ lib/comments/actions.ts
@@ -1,31 +1,21 @@
-"use server";
+'use server';
 
 import { supabaseAdmin } from '@/lib/supabaseAdmin';
 import { getServerSession } from 'next-auth/next';
 import { authOptions } from '../../app/api/auth/[...nextauth]/route';
 
 export async function createComment(
-  jamId: string, 
-  content: string, 
-  parentId: string | null = null // Default values must be at the end
+  jamId: string,
+  content: string,
+  parentId: string | null = null, // Default values must be at the end
 ) {
-
-
-
-  
   const session = await getServerSession(authOptions);
 
   if (!session?.user?.email) {
     return { error: 'Unauthorized', status: 401 };
   }
 
-
-  
-
   const userEmail = session.user.email;
-
-
-
 
   const { error } = await supabaseAdmin.rpc('insert_comment', {
     p_jam_id: jamId,
@@ -34,37 +24,45 @@ export async function createComment(
     p_email: userEmail,
   });
 
-
-  
-
   if (error) {
-    
     console.log(error);
     return { error: error.message, status: 500 };
-
   }
 
   return { success: true };
 }
 
-
 /**
  * EDIT A COMMENT
  */
-export async function editComment(commentId: string, newContent: string, jamId: string) {
+export async function editComment(
+  commentId: string,
+  newContent: string,
+  jamId: string,
+) {
+  const session = await getServerSession(authOptions);
 
+  if (!session?.user?.email) {
+    return { error: 'Unauthorized', status: 401 };
+  }
+
+  const userEmail = session.user.email;
+
+  const { error } = await supabaseAdmin.rpc('edit_user_comment', {
+    p_comment_id: commentId,
+    p_jam_id: jamId,
+    p_email: userEmail,
+    p_content: newContent,
+  });
 
   
 
+  if (error) {
+    console.log(error);
+    return { error: error.message, status: 500 };
+  }
 
-  const { error } = await supabaseAdmin
-    .from('comments')
-    .update({ content: newContent, updated_at: new Date().toISOString() })
-    .eq('jam_id', jamId)
-    .eq('id', commentId);
-
-  if (error) throw new Error(error.message);
-
+  return { success: true };
 }
 
 /**
@@ -93,7 +91,6 @@ export async function deleteComment(commentId: string, jamId: string) {
   return { success: true };
 }
 
-
 export async function restoreComment(commentId: string, jamId: string) {
   const session = await getServerSession(authOptions);
 
@@ -117,24 +114,17 @@ export async function restoreComment(commentId: string, jamId: string) {
   return { success: true };
 }
 
-
 /**
  * REPORT A COMMENT
  */
 export async function reportComment(commentId: string, jamId: string) {
-
-  
-
-const session = await getServerSession(authOptions);
+  const session = await getServerSession(authOptions);
 
   if (!session?.user?.email) {
     return { error: 'Unauthorized', status: 401 };
   }
 
   const userEmail = session.user.email;
-
-
-
 
   const { error } = await supabaseAdmin.rpc('report_user_comment', {
     p_comment_id: commentId,
@@ -147,18 +137,5 @@ const session = await getServerSession(authOptions);
     return { error: error.message, status: 500 };
   }
 
-
-
   return { success: true };
 }
-
-
-
-
-
-
-
-
-
-
-
